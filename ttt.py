0304ec1948adf972a8737cdc523a6c17774d217f
Akshay Gulabrao <aksgula22@gmail.com>
2022-10-23 20:29:29 -0400

    wrote minimax pseudocode

--- ttt.py
+++ ttt.py
@@ -1,8 +1,10 @@
 from board import Board
 from randomPlayer import RandomPlayer
 from alphaBetaPlayer import alphaBetaPlayer
-p1 = alphaBetaPlayer()
-p2 = RandomPlayer()
-b = Board(num_games = 1,player1 = p1,player2=p2)
+
+p2 = alphaBetaPlayer()
+p1 = RandomPlayer()
+
+b = Board(num_games = 100_000,player1 = p1,player2=p2)
 b.playGames()
 print(p1.num_wins,p2.num_wins,p1.num_draws)
